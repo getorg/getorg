7d58ecb412e44296f9f124659ac1f1523f51cda5
staeiou <sgeiger@gmail.com>
2016-07-26 14:06:35 -0700

    update version

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
 setup(name='getorg',
-      version='0.01',
+      version='0.1.4',
       description='For mapping contributors to GitHub organizations',
       url='http://github.com/getorg/getorg',
       author='Stuart Geiger',
       author_email='stuart@stuartgeiger.com',
       license='MIT',
       packages=['getorg'],
+      install_requires=[
+          'pygithub', 'ipywidgets', 'geopy'],
       zip_safe=False)
