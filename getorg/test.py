9f83343945dc3dfd0bda72abcf523879b5af2e4c
staeiou <sgeiger@gmail.com>
2016-07-28 16:11:33 -0700

    fix test.py initialization

--- getorg/test.py
+++ getorg/test.py
@@ -1,8 +1,7 @@
 def test_map_org():
     import getorg
     from github import Github
-    import ghlogin
-    gh = Github(login_or_token=ghlogin.gh_key)
+    gh = Github()
 
     
     map_obj, loc_dict, metadata_dict = getorg.orgmap.map_org(gh,"getorg-test")
@@ -15,11 +14,10 @@ def test_map_org():
 def test_map_orgs():
     import getorg
     from github import Github
-    import ghlogin
-    gh = Github(login_or_token=ghlogin.gh_key)
+    gh = Github()
 
     map_obj, loc_dict, metadata_dict = getorg.orgmap.map_orgs(gh,['trace-ethnography', 'getorg-test', 'getorg'])
-    
+
     assert loc_dict['https://api.github.com/users/getorg-testacct'].longitude == 0.0
     assert round(loc_dict['https://api.github.com/users/staeiou'].longitude,2) == -122.27
     assert len(loc_dict) == 2
