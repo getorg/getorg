1797ccc7f89aa0e313e0a5898fd59ed0e0c0b2d2
Stuart Geiger <sgeiger@gmail.com>
2016-08-19 14:22:46 -0700

    v 0.3.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='getorg',
-      version='0.3.0',
+      version='0.3.1',
       description='Information and analytics about GitHub organizations',
       url='http://github.com/getorg/getorg',
       author='Stuart Geiger',
